fa420510603c1088db541a8a7fee326964cbc99b
Thomas Wang <iothomaswang@gmail.com>
2019-02-20 19:17:46 -0500

    add header for hello

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,6 +1,10 @@
 exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
+        },
         body: "Hello, World"
     });
 }
